0a539f84b3bfb129865fa12ac656b80ebda61191
ChinosoNnadi <140746420+ChinosoNnadi@users.noreply.github.com>
2023-09-26 20:12:19 +0100

    Add files via upload

---
+++ Homework.java
@@ -0,0 +1,46 @@
+package Project;
+/*
+* Selenium Homework Assignment: Automating a Login Process
+Objective: Write a Selenium script to automate the login process on the OrangeHRM website.
+Instructions:
+Go to the OrangeHRM login page: https://opensource-demo.orangehrmlive.com/web/index.php/auth/login
+Enter the username "abc".
+Clear the username field.
+Enter the username "Admin".
+Enter the password "admin123".
+Get the text from the login button web element and print it on the console.
+Click on the login button.
+*/
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Homework {
+        public static void main(String[] args) throws InterruptedException {
+            WebDriver driver = new ChromeDriver();
+            driver.get( "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+            driver.manage().window().maximize();
+            Thread.sleep(2500);
+            //Enter the username "abc".
+            WebElement userName = driver.findElement(By.name("username"));
+            userName.sendKeys("abc");
+            Thread.sleep(2000);
+            userName.sendKeys(Keys.CONTROL + "a");
+            userName.sendKeys(Keys.DELETE);
+            //Clear the username field.
+            userName.clear();
+            Thread.sleep(2000);
+            //Enter the username "Admin".
+            userName.sendKeys("Admin");
+            //Enter the password "admin123".
+            WebElement password = driver.findElement(By.name("password"));
+            password.sendKeys("admin123");
+            //text from the login button web element
+            WebElement loginBtn = driver.findElement(By.tagName("button"));
+            System.out.println(loginBtn.getText());
+            loginBtn.click();
+        }
+    }
+
